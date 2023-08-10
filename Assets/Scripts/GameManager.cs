85bb8d665fbf708b7a7e4f944f06ecf722836433
Aaron <aaron.zml331@gmail.com>
2023-08-10 14:18:51 -0600

    upload GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+
+    public int currentWave = 1;
+
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get { return _instance; }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
